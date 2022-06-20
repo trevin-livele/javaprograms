c1a40755e68b7fc0e5cb3a7e1ca059bf7d731aaf
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-06-20 12:02:50 +0300

    completed min and max challenge

---
+++ arrays/Minmaxchallenge/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter count");
+        int count = scanner.nextInt();
+        scanner.nextLine();
+
+        int [] returnedArray = readingIntegers(count);
+        int returnedMin = findMin(returnedArray);
+
+        System.out.println("Min =" + returnedMin);
+
+
+
+    }
+
+    private  static int[] readingIntegers(int count){
+        int [] array = new int[count];
+
+        for (int i = 0; i< array.length; i++){
+            System.out.println("Enter a number:");
+            int number = scanner.nextInt();
+            scanner.nextLine();
+            array[i] = number;
+        }
+        return array;
+    }
+    private  static  int findMin(int[] array){
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<array.length; i++){
+            int value = array[i];
+
+            if (value < min){
+                min = value;
+            }
+
+        }
+        return min;
+    }
+}
