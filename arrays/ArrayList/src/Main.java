12191b4117e7bf7c544a11ed7e5d53294f2b9b0a
trevin <liveletrevin6@gmail.com>
2022-07-07 15:31:20 +0300

    added class and methods

---
+++ arrays/ArrayList/src/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static  Grocery groceryList = new Grocery();
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        int choice = 0;
+        printInstructions();
+        while(!quit){
+            System.out.println("Enter your choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+                case 0:
+                    printInstructions():
+                    break;
+                case 1:
+                    groceryList.printGroceryList();
+                    break;
+                case 2:
+                    addItem();
+                    break;
+                case 3:
+                    modifyItem();
+                    break;
+                case 4:
+                    removeItem();
+                    break;
+                case 5:
+                    searchForItem();
+                    break;
+                case 6:
+                    quit = true;
+                    break;
+
+
+            }
+        }
+
+
+    }
+
+    public static void printInstructions(){
+        System.out.println("\nPress ");
+        System.out.println("\t 0 - To print choice options");
+        System.out.println("\t 1 - To print a list of grocery items");
+        System.out.println("\t 2 - To add an item to the list,");
+        System.out.println("\t 3 - To modify the item in the list");
+        System.out.println("\t 4 - To remove the item in the list.");
+        System.out.println("\t 5 - To search for an item in the list");
+        System.out.println("\t 6 - To quit the application");
+        System.out.println("\t ---------------------------------------------------");
+
+    }
+
+    public static void void
+}
+
+
+
+
+
+
+
+
+
+
